ce517be8661853a2e2345d35cdf1279a360f54ac
Rutmank <rutmank@yandex.ru>
2020-10-13 15:21:24 +0300

    Refactoring

--- SimulationOfLife/Form1.cs
+++ SimulationOfLife/Form1.cs
@@ -12,12 +12,9 @@ namespace SimulationOfLife
 {
     public partial class Form1 : Form
     {
-        private int currentGeneration = 0;
         private Graphics graphics;
         private int resolution;
-        private bool[,] field; // создание игрового поля ( таблица из клеток ), определение по типу bool
-        private int rows;
-        private int cols;
+        private Engine gameEngine;
 
         public Form1()
         {
@@ -31,78 +28,42 @@ namespace SimulationOfLife
             if (timer1.Enabled) // после нажатия кнопки, возможность регулировать параметры блокируется
                 return;
 
-            currentGeneration = 0;
-            Text = $"Generation{currentGeneration}"; // Подсчет поколений
-
             nudResolution.Enabled = false; // Блокировка кнопок
             nudDensity.Enabled = false;
 
             resolution = (int)nudResolution.Value; // Присваивание значения из numeric
-            rows = pictureBox1.Height / resolution; // Значение поля, деленое на разрешение ( строки )
-            cols = pictureBox1.Width / resolution;
-            field = new bool[cols, rows]; // Создание массива ( поля ) по декартовой системе
 
-            Random random = new Random(); // Объект, генерирующий случайные числа
-            for (int x = 0; x < cols; x++) // генератор случайных чисел. Перебор всех элементов по осям
-            {
-                for (int y = 0; y < rows; y++)
-                {
-                    field[x, y] = random.Next((int)nudDensity.Value) == 0; // Если сгенерирован 0, то true и будет сгенерирована клетка. Чем больше density, тем больше диапазон случайности
-                }
-            }
+            gameEngine = new Engine
+            (
+                rows: pictureBox1.Height / resolution, // Значение поля, деленое на разрешение ( строки )
+                cols: pictureBox1.Width / resolution, // :  Присваивание. Именнованный параметр 
+                density: (int)nudDensity.Minimum + (int)nudDensity.Maximum - (int)nudDensity.Value // Исправление количества клеток. 
+            );
+
+            Text = $"Generation{gameEngine.CurrentGeneration}"; // Подсчет поколений
 
             pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height); // Создается картинка(Bitmap), соответствующая размеру boxa
             graphics = Graphics.FromImage(pictureBox1.Image); // Создается объект, в который в качестве параметра передается созданное изображение (строчка-посредник)
             timer1.Start(); // Старт таймера
         }
 
-        private void NextGeneration() // Генерация нового поколения
+        private void DrawNextGeneration() // Генерация нового поколения
         {
             graphics.Clear(Color.SeaGreen); // Заливка цвета поля
 
-            var newField = new bool[cols, rows]; // Извлечение старых данных в новый массив, который будет генерировать новое поколение. Старый массив не изменяется
+            var field = gameEngine.GetCurrentGeneration();
 
-            for (int x = 0; x < cols; x++)
+            for (int x = 0; x < field.GetLength(0); x++)
             {
-                for (int y = 0; y < rows; y++)
+                for (int y = 0; y < field.GetLength(1); y++)
                 {
-                    var neighboursCount = CountNeighbours(x, y); // Переменная для подсчета соседей, у рассматриваемого элемента
-                    var hasLife = field[x, y]; // Перемення, проверяющая, есть ли живая клетка по координатам
-
-                    if (!hasLife && neighboursCount == 3)
-                        newField[x, y] = true; // Создание клетки, если 3 соседа
-                    else if (hasLife && (neighboursCount<2 || neighboursCount >3))
-                        newField[x, y] = false; // Клетка умирает
-                    else
-                        newField[x, y] = field[x, y]; // Клетка остается жить. Изменений нет
-
-                    if (hasLife)
-                        graphics.FillRectangle(Brushes.Crimson, x * resolution, y * resolution, resolution -1, resolution -1); // Отрисовка прямоугольника. В качестве отступов - индекс массива * разрешение
+                    if(field[x,y])
+                        graphics.FillRectangle(Brushes.Crimson, x * resolution, y * resolution, resolution - 1, resolution - 1); // Отрисовка прямоугольника. В качестве отступов - индекс массива * разреш
                 }
             }
-            field = newField; // Пересоздание массива с новыми данными
             pictureBox1.Refresh();// Перерисовка игрового поля 
-            Text = $"Generation{++currentGeneration}";
-        }
-
-        private int CountNeighbours(int x, int y)
-        {
-            int count = 0;
-            for (int i = -1; i < 2; i++) // подсчет соседей для обеих координат
-            {
-                for (int j = -1; j < 2; j++)
-                {
-                    var col = (x + i + cols) % cols; // Круглость планеты
-                    var row = (y + j + rows) % rows;
-
-                    var isSelfChecking = col == x && row == y; // Проверка самого себя
-                    var hasLife = field[col, row]; // Если клетка жива, ее значение помещается в массив
-
-                    if (hasLife && !isSelfChecking) // Если есть жизнь, но это не сама клетка, значит это живой сосед
-                        count++;
-                }
-            }
-            return count;
+            Text = $"Generation{gameEngine.CurrentGeneration}"; // отображение счетчика поколений
+            gameEngine.NextGeneration(); //  Генерация следующего поколения
         }
 
         private void Stop() // Остановка таймера по кнопке. Доступ к изменению параметров
@@ -114,7 +75,6 @@ namespace SimulationOfLife
             nudDensity.Enabled = true;
         }
 
-
         private void splitContainer1_SplitterMoved(object sender, SplitterEventArgs e)
         {
 
@@ -122,7 +82,7 @@ namespace SimulationOfLife
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            NextGeneration();
+            DrawNextGeneration();
         }
 
         private void bStart_Click(object sender, EventArgs e)
@@ -140,33 +100,24 @@ namespace SimulationOfLife
             if (!timer1.Enabled)
                 return;
 
-            if (e.Button == MouseButtons.Left) // Управление мышкой
+            if (e.Button == MouseButtons.Left) // Управление мышкой. Добавление клетки
             {
                 var x = e.Location.X / resolution;
                 var y = e.Location.Y / resolution;
-                var validationPassed = ValidateMouse(x, y);
-                if (validationPassed)
-                    field[x, y] = true;
+                gameEngine.AddCell(x, y);
             }
 
-            if (e.Button == MouseButtons.Right) // Управление мышкой
+            if (e.Button == MouseButtons.Right) // Управление мышкой. Удаление клетки
             {
                 var x = e.Location.X / resolution;
                 var y = e.Location.Y / resolution;
-                var validationPassed = ValidateMouse(x, y);
-                if (validationPassed)
-                    field[x, y] = false;
+                gameEngine.DelCell(x, y);
             }
         }
 
-        private bool ValidateMouse(int x, int y) // Валидатор мыши 
-        {
-            return x >= 0 && y >= 0 && x < cols && y < rows;
-        }
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
-            Text = $"Generation{currentGeneration}";
-        }
+        
+
+
     }
 }
